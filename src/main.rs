21e5dbe7c5b1c7979cb0b0c22a742b09938218fd
koto74 <kotoganaiyo@gmail.com>
2024-03-01 19:00:10 +0900

    feat: ballを描画する

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    sprite::MaterialMesh2dBundle,
+};
+
+const BALL_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
+const BALL_STARTING_POSITION: Vec3 = Vec3::new(0.0, -50.0, 1.0);
+const BALL_DIAMETER: f32 = 30.0;
+const BALL_SPEED: f32 = 400.0;
+const INITIAL_BALL_DIRECTION: Vec2 = Vec2::new(0.5, -0.5);
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins) // ウィンドウやスケジュールなどの基本的な機能を追加
-        .add_systems(
-            Startup,
-            (
-                spawn_camera2d, // 2Dカメラを追加
-                spawn_text2d,   // 2Dテキストを追加
-            )
-        )
+        .add_systems(Startup, setup)
         .add_systems(
             Update,
             (
@@ -19,12 +22,38 @@ fn main() {
         .run();
 }
 
-fn spawn_camera2d(mut cmds: Commands) {
-    cmds.spawn(Camera2dBundle::default());
-}
+#[derive(Component)]
+struct Ball;
 
-fn spawn_text2d(mut cmds: Commands) {
-    let textstyle = TextStyle { font_size: 100.0, ..default() };
-    let text = Text::from_section("Hello, world!", textstyle);
-    cmds.spawn(Text2dBundle { text, ..default() });
+#[derive(Component)]
+struct Velocity(Vec2);
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    // Camera
+    commands.spawn(Camera2dBundle::default());
+
+    // Ball
+    commands.spawn((
+        // 2Dメッシュとマテリアルを束ねたもの
+        // 2Dメッシュは円形のメッシュを作成
+        // マテリアルはボールの色を指定
+        // トランスフォームはボールの位置と大きさを指定
+        // その他のフィールドはデフォルト値
+        MaterialMesh2dBundle {
+            mesh: meshes.add(Circle::default()).into(),
+            material: materials.add(BALL_COLOR),
+            transform: Transform::from_translation(BALL_STARTING_POSITION)
+                // ボールの大きさを指定
+                .with_scale(Vec2::splat(BALL_DIAMETER).extend(1.0)),
+            ..default()
+        },
+        Ball,
+        Velocity(INITIAL_BALL_DIRECTION.normalize() * BALL_SPEED),
+    ));
+
 }
